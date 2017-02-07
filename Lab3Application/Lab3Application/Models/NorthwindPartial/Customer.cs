3c12e63ac7ee66ea28e77b73a2e4f06a9b8261e0
tjang <tjang@fusiongenomics.com>
2017-02-06 21:41:01 -0800

    first commit

---
+++ Lab3Application/Lab3Application/Models/NorthwindPartial/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab3Application.Models.Northwind
+{
+    [MetadataType(typeof(CustomerMetaData))]
+    public partial class Customer { }
+    public partial class CustomerMetaData
+    {
+        [Required]
+        public string CustomerID { get; set; }
+
+        [Display(Name = "Company")]
+        [Required]
+        public string CompanyName { get; set; }
+
+        [Required]
+        [Display(Name = "Comtact")]
+        public string ContactName { get; set; }
+
+        [Required]
+        public string ContactTitle { get; set; }
+
+        public string Address { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Region { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string Country { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Fax { get; set; }
+    }
+}
---
+++ Lab3Application/Lab3Application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab3Application.Startup))]
+namespace Lab3Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
